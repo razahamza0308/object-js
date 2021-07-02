ca187f81c42f370fe94d49b4ebe6aa4a26ea9b72
razahamza0308 <65039022+razahamza0308@users.noreply.github.com>
2021-07-03 00:13:21 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,88 @@
+// *****************Question 1 ************************/
+
+// var itemsArray=[
+//     {
+//         name:"Juice",
+//         price: 50,
+//         Quantity: 3,
+//     },
+//     {
+//         name:"Cookies",
+//         price: 30,
+//         Quantity: 9,
+//     },
+//     {
+//         name:"Shirt",
+//         price: 880,
+//         Quantity: 1,
+//     },
+//     {
+//         name:"Pen",
+//         price: 110,
+//         Quantity: 2,
+//     }
+// ]
+
+
+
+// for (var i=0;i<itemsArray.length;i++){
+//     var tot=itemsArray[i].price*itemsArray[i].Quantity
+//     console.log("Total Price of "+itemsArray[i].name+" is "+tot)
+   
+
+// }
+
+// console.log("Net price is "+(itemsArray[0].price+itemsArray[1].price+itemsArray[2].price+itemsArray[3].price))
+
+
+//*****************Question 2 ************************/
+
+// var deta={
+//     name:"Ghous",
+//     email:"Ghous@gmail.com",
+//     password:"*****",
+//     age:"21",
+//     gender:"Male",
+//     City:"Karachi",
+//     Country:"Pakistan"
+// }
+
+// console.log("age" in deta)
+// console.log("Country" in deta)
+// console.log("firstName" in deta)
+// console.log("lastName" in deta)
+
+//*****************Question 3 ************************/
+
+// function Detal(name,email,age,gender,city,country){
+//     this.name=name
+//     this.email=email
+//     this.age=age
+//     this.gender=gender
+//     this.city=city
+//     this.country=country
+// }
+
+// var students=[
+// new Detal("ghous","Ghous@gmail.com","21","male","karachi","Pakistan"),
+// new Detal("Ahmed","Ahmed@gmail.com","23","male","karachi","Pakistan"),
+// new Detal("Ali","Ali@gmail.com","19","male","karachi","Pakistan")
+
+// ]
+
+// for(i=0;i<=students.length;i++){
+// console.log(students[i])}
+
+
+//*******************Question 4 ****************************/
+
+
+
+
+
+
+
+
+
+
+
